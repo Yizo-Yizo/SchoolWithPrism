a580ffc3029c29d6fa7918b1e33a2a25a8eafd43
Yizo-Yizo <56066066+Yizo-Yizo@users.noreply.github.com>
2019-12-05 16:20:08 +0200

    SignUp not complete

---
+++ SchoolFinder/SchoolFinder/SchoolFinder/ServiceHandler/LoginService.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+using SchoolFinder.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolFinder.ServiceHandler
+{
+    class LoginService
+    {
+        RestClient<User> restClient = new RestClient<User>();
+        public async Task<bool> CheckLoginIfExists(string email, string password)
+        {
+            var check = await _restClient.checkLogin(email, password);
+            return check;
+        }
+    }
+}
